75f3da7ed91e3d7dccacaccfb1189532c3693adb
sejal <sejal4789@gmail.com>
2023-05-04 10:43:59 +0530

    hai

--- Netflix/ViewController.swift
+++ Netflix/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   var staticUserName = "Sejal"
+    var staticPassword = "Sejal@2114"
+    @IBOutlet weak var userNameText: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
+    @IBAction func SignInButton(_ sender: Any) {
+        let username = userNameText.text!
+        let password = passwordTextField.text!
+        
+        if username == "" {
+            print("username is empty")
 
+        }
+        
+        if username == staticUserName && password == staticPassword {
+            print("success")
+        }
+        else {
+           print("Failed")
+        }
+    }
+    
 }
 
